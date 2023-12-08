9351c7bc955e3de6e0d7a933baa5a17db4e908c3
rahairston <rahairston2014@verizon.net>
2023-12-07 21:35:50 -0600

    undid too much

--- streamer.py
+++ streamer.py
@@ -7,6 +7,7 @@
 import io
 import logging
 import socketserver
+import socket
 from http import server
 from threading import Condition
 from picamera2.encoders import JpegEncoder
@@ -37,28 +38,28 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Content-Length', len(content))
             self.end_headers()
             self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with self.server.output.condition:
-                        self.server.output.condition.wait()
-                        frame = self.server.output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
+        # elif self.path == '/stream.mjpg':
+        #     self.send_response(200)
+        #     self.send_header('Age', 0)
+        #     self.send_header('Cache-Control', 'no-cache, private')
+        #     self.send_header('Pragma', 'no-cache')
+        #     self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+        #     self.end_headers()
+        #     try:
+        #         while True:
+        #             with self.server.output.condition:
+        #                 self.server.output.condition.wait()
+        #                 frame = self.server.output.frame
+        #             self.wfile.write(b'--FRAME\r\n')
+        #             self.send_header('Content-Type', 'image/jpeg')
+        #             self.send_header('Content-Length', len(frame))
+        #             self.end_headers()
+        #             self.wfile.write(frame)
+        #             self.wfile.write(b'\r\n')
+        #     except Exception as e:
+        #         logging.warning(
+        #             'Removed streaming client %s: %s',
+        #             self.client_address, str(e))
         else:
             self.send_error(404)
             self.end_headers()
