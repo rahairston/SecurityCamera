a10b5892980bc0042bd19f5dcbdce2f316dc6f22
rahairston <rahairston2014@verizon.net>
2023-12-09 00:22:26 -0600

    resigning to tcp stream for now. Works on VLC and I doubt I'll do any actual stream on my use

--- streamer.py
+++ streamer.py
@@ -1,86 +1,15 @@
 #!/usr/bin/python3
 
-# Mostly copied from https://picamera.readthedocs.io/en/release-1.13/recipes2.html
-# Run this script, then point a web browser at http:<this-ip-address>:8000
-# Note: needs simplejpeg to be installed (pip3 install simplejpeg).
-
-import io
-import logging
-import socketserver
 import socket
-from http import server
-from threading import Condition
 import threading
 from picamera2.outputs import FileOutput
 
-PAGE = """\
-<html>
-<head>
-<title>picamera2 MJPEG streaming demo</title>
-</head>
-<body>
-<h1>Picamera2 MJPEG Streaming Demo</h1>
-<img src="stream.mjpg" width="640" height="480" />
-</body>
-</html>
-"""
-
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = self.server.web_page.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with self.server.output.condition:
-                        self.server.output.condition.wait()
-                        frame = self.server.output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
-
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
-
 # Class that is responsible for streaming the camera footage to the web-page.
 class Streamer:
-    def __init__(self, camera, streamer_output,recorder_output, port=8000):
+    def __init__(self, camera, streamer_output, port=8000):
         self.camera = camera
         self.streamer_output=streamer_output
         self.port = port
-        self.recorder_output=recorder_output
-
-    def get_ip(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 0))
-        return s.getsockname()[0]
    
     # Start streaming.
     def start(self):
@@ -98,11 +27,5 @@ class Streamer:
                     self.streamer_output.start()
                     self.streamer_output.connectiondead = lambda _: event.set()  # noqa
                     event.wait()
-            # self.streamer_output.start()
-            # address = ('', self.port)
-            # sv = StreamingServer(address, StreamingHandler)
-            # sv.output = self.streamer_output.fileoutput
-            # sv.web_page = PAGE.format(self.get_ip(), self.port)
-            # sv.serve_forever()
         except KeyboardInterrupt:
             self.camera.close()
