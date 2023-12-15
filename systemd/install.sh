b22bb96e0dab751bb010482db1920ec12b5d68d1
rahairston <rahairston2014@verizon.net>
2023-12-15 09:20:09 -0600

    Fixing some systemd shenanigans

--- systemd/install.sh
+++ systemd/install.sh
@@ -3,7 +3,7 @@ USER_ID=$USER
 
 sed "s/%i/$USER/" security-camera.template > security-camera.service
 
-mkdir -p ~/.config/systemd/
+mkdir -p ~/.config/systemd/user/
 mv security-camera.service ~/.config/systemd/
 
 systemctl --user daemon-reload
