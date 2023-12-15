f8f0efd0037d9abb4b627a574fa5e898e43fda66
rahairston <rahairston2014@verizon.net>
2023-12-15 09:20:58 -0600

    oops

--- systemd/install.sh
+++ systemd/install.sh
@@ -4,7 +4,7 @@ USER_ID=$USER
 sed "s/%i/$USER/" security-camera.template > security-camera.service
 
 mkdir -p ~/.config/systemd/user/
-mv security-camera.service ~/.config/systemd/
+mv security-camera.service ~/.config/systemd/user/
 
 systemctl --user daemon-reload
 systemctl --user --now enable security-camera.service
