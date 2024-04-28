8b11a24ffb84a4c7cb13daea9deece4788504075
rahairston <rahairston2014@verizon.net>
2024-04-28 16:59:11 -0500

    prefix

--- recorder.py
+++ recorder.py
@@ -60,7 +60,10 @@ class Recorder:
                               + '-' + str(datetime.datetime.now())[17:19]
         if not os.path.isdir(os.path.join(get_exec_dir(), self.temporary_recordings_output_path)):
             os.mkdir(os.path.join(get_exec_dir(), self.temporary_recordings_output_path))
-        output_file_name = os.path.join(get_exec_dir(), self.temporary_recordings_output_path, self.storage.prefix, current_time_string) + ".h264"
+        prefix = self.storage.prefix
+        if prefix != "":
+            prefix = prefix+"_"
+        output_file_name = os.path.join(get_exec_dir(), self.temporary_recordings_output_path, prefix, current_time_string) + ".h264"
         self.delayed_recording_stream.fileoutput = output_file_name
         self.delayed_recording_stream.start()
         print('Started recording '+output_file_name)
